216bd732767267660957b277c4c20f9d8f270f24
AkashSinha007 <akashkumarsinha007@gmail.com>
2023-12-28 11:10:34 +0100

    Tutorial_1 Complete

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
 from PyQt5.QtWidgets import *
 
+from node_editor_wnd import NodeEditorWnd
+
 if __name__ == '__main__':
     # print("Hello World")
     app = QApplication(sys.argv);
 
-    label = QLabel("Hello , PyQt5 !");
-    label.show();
+    wnd = NodeEditorWnd();
 
     sys.exit(app.exec_());
