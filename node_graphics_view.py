f05f885b79b075f7b03c16d4d9316342ba54e6ad
AkashSinha007 <akashkumarsinha007@gmail.com>
2023-12-29 23:42:20 +0100

    Tutorial_3 Complete

--- node_graphics_view.py
+++ node_graphics_view.py
@@ -10,6 +10,12 @@ class QDMGraphicsView(QGraphicsView):
         self.initUI();
 
         self.setScene(self.grScene);
+    
+        self.zoomInFactor = 1.25;
+        self.zoomClamp = False;
+        self.zoom = 10;
+        self.zoomStep = 1;
+        self.zoomRange = [0, 10];
 
     def initUI(self):
         self.setRenderHints(QPainter.Antialiasing |QPainter.HighQualityAntialiasing | QPainter.TextAntialiasing | QPainter.SmoothPixmapTransform );
@@ -18,3 +24,70 @@ class QDMGraphicsView(QGraphicsView):
 
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff);
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff);
+    
+        self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse);
+
+    def mousePressEvent(self,event):
+        if event.button() == Qt.MiddleButton:
+            self.middleMouseButtonPress(event);
+        elif event.button() == Qt.LeftButton:
+            self.leftMouseButtonPress(event);
+        elif event.button() == Qt.RightButton:
+            self.rightMouseButtonPress(event);
+        else:
+            super().mousePressEvent(event);
+
+    def mouseReleaseEvent(self,event):
+        if event.button() == Qt.MiddleButton:
+            self.middleMouseButtonRelease(event);
+        elif event.button() == Qt.LeftButton:
+            self.leftMouseButtonRelease(event);
+        elif event.button() == Qt.RightButton:
+            self.rightMouseButtonRelease(event);
+        else:
+            super().mouseReleaseEvent(event);
+
+    def middleMouseButtonPress(self,event):
+        releaseEvent = QMouseEvent(QEvent.MouseButtonRelease, event.localPos(), event.screenPos(),
+                                   Qt.LeftButton, Qt.NoButton, event.modifiers());
+        super().mouseReleaseEvent(releaseEvent);
+        self.setDragMode(QGraphicsView.ScrollHandDrag);
+        fakeEvent = QMouseEvent(event.type(), event.localPos(), event.screenPos(),
+                                Qt.LeftButton, event.buttons() | Qt.LeftButton, event.modifiers());
+        super().mousePressEvent(fakeEvent);
+
+
+    def middleMouseButtonRelease(self,event):
+        fakeEvent = QMouseEvent(event.type(), event.localPos(), event.screenPos(),
+                                Qt.LeftButton, event.buttons() & ~Qt.LeftButton, event.modifiers());
+        super().mouseReleaseEvent(fakeEvent);
+        self.setDragMode(QGraphicsView.NoDrag);
+
+
+    def leftMouseButtonPress(self, event):
+        return super().mousePressEvent(event);
+
+    def leftMouseButtonRelease(self, event):
+        return super().mouseReleaseEvent(event);
+
+    def rightMouseButtonPress(self, event):
+        return super().mousePressEvent(event);
+
+    def rightMouseButtonRelease(self, event):
+        return super().mouseReleaseEvent(event);
+
+    def wheelEvent(self, event):
+        # calculate our zoom Factor
+        zoomOutFactor = 1 / self.zoomInFactor;
+
+        # calculate zoom
+        if event.angleDelta().y() > 0:
+            zoomFactor = self.zoomInFactor;
+            self.zoom += self.zoomStep;
+        else:
+            zoomFactor = zoomOutFactor;
+            self.zoom -= self.zoomStep;
+
+
+        # set scene scale
+        self.scale(zoomFactor, zoomFactor);
